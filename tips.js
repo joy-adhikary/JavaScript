1702c12ea457bbd8b97a65402061b156b76f47cc
Joy Adhikary <91140113+joy-adhikary@users.noreply.github.com>
2023-01-04 12:55:38 +0600

    added tips

---
+++ tips.js
@@ -0,0 +1,9 @@
+function sum(...joy){
+    let total=0;
+    for(let i of joy){total+=i}
+  return total;
+}
+// issa moto number pass korty parbo ... er jonno 
+console.log(sum(1,2,3,4))
+console.log(sum(1,4))
+console.log(sum(1,2,4))
