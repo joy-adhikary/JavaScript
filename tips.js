a0cd9e8200d69e26e55b07085a27ae7d7d6a0b5f
Joy Adhikary <91140113+joy-adhikary@users.noreply.github.com>
2023-02-10 20:36:20 +0600

    Add 5 6 7

--- tips.js
+++ tips.js
@@ -23,17 +23,43 @@ console.log(joy) // joy 00
 const joy={ name: " joy ", id:62 }
 const roy=joy;
 roy.id=55;
-console.log(joy) // it will change the value of joy.id also.
+console.log(joy) //  name:  joy , id: 55 that means it will change the value of joy.id also.
 
-to avoid it we can use  this ( ... )
+to avoid it we can use  this ( ... ) spred oparetor 
 const joy={ name: " joy ", id:62 }
 const roy={...joy};
 roy.id=00;
 console.log(joy) // it will not change joy.id 
 
 
+// 4.Turnary oparetor 
 
-// 4.
+let x=true;
+if x?console.log("its true") : console.log("false").    //if condition ? ( if ture then execute this block ) : ( if false then execute this block )
 
+
+
+5. const Obj = {
+  data:{
+      
+      name : "JOY",
+      id: "0978",
+      class: "12",
+  }
+}
+const name=obj.data.name , const id=obj.data.id,const class =obj.data.class;
+eitar equivalent holo 
+const {name,id,class}=Obj.data;
+
+
+7.assigning value in a short way 
+
+let x;
+if (data.name){
+    x=data.name}
+else x=6666;
+
+eitar equivalent holo 
+let x=data.name || 6666
 
 
